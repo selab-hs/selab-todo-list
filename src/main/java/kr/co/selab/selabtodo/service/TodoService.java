44c65172d03b59188ef724640d089f062c5c0bbe
xx10222 <yjw4960@gmail.com>
2022-09-23 23:44:27 +0900

    :recycle: 삭제 리팩토링

--- src/main/java/kr/co/selab/selabtodo/service/TodoService.java
+++ src/main/java/kr/co/selab/selabtodo/service/TodoService.java
@@ -3,16 +3,12 @@ package kr.co.selab.selabtodo.service;
 import kr.co.selab.selabtodo.error.exception.NotExistsTodoException;
 import kr.co.selab.selabtodo.model.Todo;
 import kr.co.selab.selabtodo.model.dto.CreateRequest;
-import kr.co.selab.selabtodo.model.dto.TodoRequest;
 import kr.co.selab.selabtodo.model.dto.TodosResponse;
 import kr.co.selab.selabtodo.model.dto.UpdateRequest;
 import kr.co.selab.selabtodo.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
-
 
 @RequiredArgsConstructor
 @Service
@@ -31,12 +27,12 @@ public class TodoService {
                 .build();
     }
 
-    // 투두리스트에 아이템 추가
+    @Transactional
     public Todo createTodo(CreateRequest request) {
         return todoRepository.save(Todo.builder()
                         .title(request.title())
                         .order(request.order())
-                        .completed(false)
+                        .completed(Boolean.FALSE)
                 .build());
     }
 
@@ -47,16 +43,12 @@ public class TodoService {
         return getTodo(id);
     }
 
-    // 특정 아이템 삭제
-    public Boolean deleteTodo(Long id) {
+    @Transactional
+    public void deleteTodo(Long id) {
         todoRepository.deleteById(id);
-        if(todoRepository.findById(id).isPresent()) {
-            return false;
-        }
-        return true;
     }
 
-    // 전체 아이템 삭제
+    @Transactional
     public void deleteAll() {
         todoRepository.deleteAll();
     }
