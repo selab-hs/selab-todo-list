cbc114a72ab19f1cf8726e7b212c885416f667bb
HwangHarim <scopan01@naver.com>
2022-10-09 00:59:16 +0900

    [FIX] - 4차 리뷰사항 @ControllerAdvice 추가 하기 )

--- src/main/java/kr/co/selab/selabtodo/infrastructure/ToDoListRepository.java
+++ src/main/java/kr/co/selab/selabtodo/infrastructure/ToDoListRepository.java
@@ -1,12 +1,10 @@
 package kr.co.selab.selabtodo.infrastructure;
 
+import kr.co.selab.selabtodo.domain.ToDoList;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import java.util.List;
-import kr.co.selab.selabtodo.domain.ToDoList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ToDoListRepository extends JpaRepository<ToDoList, Long> {
   Page<ToDoList> findAll(Pageable pageable);
-//  List<ToDoList> findAllTodo(Pageable pageable);
 }
