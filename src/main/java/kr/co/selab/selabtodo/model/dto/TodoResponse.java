15427bc7bc3933a0884fddca43b4cbc774c12b5f
xx10222 <yjw4960@gmail.com>
2022-10-11 15:48:57 +0900

    :recycle: responseDto에 할일 순서(order) 컬럼 추가

--- src/main/java/kr/co/selab/selabtodo/model/dto/TodoResponse.java
+++ src/main/java/kr/co/selab/selabtodo/model/dto/TodoResponse.java
@@ -11,12 +11,14 @@ import lombok.Getter;
 public class TodoResponse {
     private Long id;
     private String title;
+    private Long order;
     private Boolean completed;
 
     public static TodoResponse of(Todo todo) {
         return TodoResponse.builder()
                 .id(todo.getId())
                 .title(todo.getTitle())
+                .order(todo.getOrder())
                 .completed(todo.getCompleted())
                 .build();
     }
